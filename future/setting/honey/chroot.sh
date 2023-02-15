1f3832dc8004cd35c56627098e2547da3797473d
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-02-15 22:18:37 +0900

    Fixing Bug.

--- future/setting/honey/chroot.sh
+++ future/setting/honey/chroot.sh
@@ -93,7 +93,6 @@ dpkg-reconfigure -f noninteractive locales
 /usr/bin/expect<<EOF
 spawn dpkg-reconfigure -f readline resolvconf
 expect "updates?" { send "Yes\r" }
-expect "dynamic files?" { send "Yes\r" }
 EOF
 #NetworkManager
 apt-get install netplan.io
