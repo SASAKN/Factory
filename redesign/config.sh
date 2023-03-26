f1fa4da92785fce9e5a7a56520aaf367b06fa839
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-03-26 10:55:49 +0900

    Update Config

---
+++ redesign/config.sh
@@ -0,0 +1,42 @@
+#設定を読み込む
+#ベースのOSリポジトリー
+os_repository="http://jp.archive.ubuntu.com/ubuntu"
+
+#ベースのコードネーム
+os_codename="jammy"
+
+#OSの名前
+os_name="Honey Linux"
+
+#OSのCPU
+os_arch="amd64"
+
+#OSのインストールラベル
+os_label1="Try Honey Dev"
+os_label2="Install Honey Dev"
+os_label3="Check Disk"
+os_label4="Memory Test (Legacy)"
+os_label5="Memory Test(UEFI)"
+
+#ビルド番号
+buildid="A1"
+
+#設定の名前
+setting_name="honey"
+
+###Expt
+echo "これは、実験的な実装です。"
+
+#カーネルのパッケージ名
+kernel="linux-generic"
+
+#Live環境から離れた時の削除処理
+remove_package="
+    ubiquity \
+    casper \
+    discover \
+    laptop-detect \
+    os-prober \
+"
+username="honeylinux"
+hostname="honey-linux"
