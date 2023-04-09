eeb88ef0ff1ad605c27e3fbd19532e312c010b4d
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-04-09 15:37:44 +0900

    Delete Add User function

--- Expt/setting/nks/final.sh
+++ Expt/setting/nks/final.sh
@@ -2,20 +2,3 @@
 # ファイナルステップへようこそ！              　　　　　　　　　　　　　　#
 #　ここに、OS独自のファイルをコピーするなどの編集を加えてください。        #
 #################################################################
-
-apt install initramfs-tools
-cat <<EOF > /etc/casper.conf
-# This file should go in /etc/casper.conf
-# Supported variables are:
-# USERNAME, USERFULLNAME, HOST, BUILD_SYSTEM
-export USERNAME="${username}"
-export USERFULLNAME="${userfullname}"
-export HOST="${username}"
-export BUILD_SYSTEM="Ubuntu"
-export FLAVOUR="${username}"
-EOF
-chmod 775 /usr/share/initramfs-tools/scripts/casper-bottom/*adduser
-chmod 775 /usr/share/initramfs-tools/scripts/casper-bottom/*autologin
-mkinitramfs -o /boot/initrd.img-`uname -r` `uname -r`
-#終わり
-exit 0
