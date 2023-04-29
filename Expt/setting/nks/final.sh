9a7ed302655bfe6c298efdf3add9eccbe44d15d8
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-04-29 17:59:43 +0900

    Create folder

--- Expt/setting/nks/final.sh
+++ Expt/setting/nks/final.sh
@@ -2,3 +2,15 @@
 # ファイナルステップへようこそ！              　　　　　　　　　　　　　　#
 #　ここに、OS独自のファイルをコピーするなどの編集を加えてください。        #
 #################################################################
+
+#NKSOS独自の壁紙に変更。
+
+#Install wallpaper
+
+#Apply Wallpaper
+cd /root/command
+bash ./plasma-apply-wallpaperimage /usr/share/wallpapers/nks-wallpaper/contents/screenshot.png
+
+#NKSOSのブートスプラッシュに変更
+
+#NKSOSのに変更
