91e2f491d9e909b68b4f68ff513adb1ee9f7f384
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-04-29 17:52:03 +0900

    初期設定用のコマンドをコピーするように設定変更。

--- Expt/factory.sh
+++ Expt/factory.sh
@@ -26,6 +26,7 @@ sudo debootstrap \
 #設定ファイルのコピー
 sudo cp -a ${script_dir}/setting/${setting_name}/file/ ${script_dir}/chroot/root/file/
 sudo cp -a ${script_dir}/setting/${setting_name}/copyfs/ ${script_dir}/chroot/root/copyfs/
+sudo cp -a ${script_dir}/command ${script_dir}/chroot/root/command/
 sudo cp ${script_dir}/setting/${setting_name}/chroot.sh ${script_dir}/chroot/root/chroot.sh
 sudo cp ${script_dir}/setting/${setting_name}/final.sh ${script_dir}/chroot/root/final.sh
 sudo cp ${script_dir}/setting/${setting_name}/deps.list ${script_dir}/chroot/root/deps.list
