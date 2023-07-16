2d92b7823fe61b87afef4d026fb38f2cc7b064f9
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-07-16 23:04:34 +0900

    必要なソフトウェアを記述

--- Expt/setting/nks/final.sh
+++ Expt/setting/nks/final.sh
@@ -3,17 +3,24 @@
 #　ここに、OS独自のファイルをコピーするなどの編集を加えてください。        #
 #################################################################
 
-#NKSOS独自の壁紙に変更。
+#Install  Desktop Theme
+apt-get install materia-kde -y
+
+#Install Start up sounds
+apt-get install alsa-utils -y
+
+#Install Introduction movie
+apt-get install mpv -y
+
 #Install wallpaper
 cd /root/deb
 dpkg -i ./nks-wallpaper.deb
 
 #NKSOSのブートスプラッシュに変更
 cd /root/command
-update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/nksos/nksos.plymouth 100
+#update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/nksos/nksos.plymouth 100
 
 #NKSOSのブート画面に変更
 cd /root/file
 mv grub.txt grub
 cp grub /etc/default/grub
-update-grub
