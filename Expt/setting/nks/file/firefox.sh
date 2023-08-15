4da6f57e2a01ed24330c47ecf520abadc0095197
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-08-15 11:37:53 +0900

    管理者権限の定義を変更

--- Expt/setting/nks/file/firefox.sh
+++ Expt/setting/nks/file/firefox.sh
@@ -5,12 +5,12 @@ echo '
 Package: *
 Pin: release o=LP-PPA-mozillateam
 Pin-Priority: 1001
-' | sudo tee /etc/apt/preferences.d/mozilla-firefox
+' | tee /etc/apt/preferences.d/mozilla-firefox
 
-echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
+echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
 
-sudo apt update && sudo apt full-upgrade -y
+apt update && apt full-upgrade -y
 
-sudo apt install -y firefox firefox-locale-ja 
+apt install -y firefox firefox-locale-ja 
 
-sudo apt update && sudo apt autoremove -y
+apt update && apt autoremove -y
